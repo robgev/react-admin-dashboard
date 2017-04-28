ae67bb201888b92a7727f6834eae1386320a792e
Gohar <goharmkrtchian9@gmail.com>
2017-04-28 17:22:51 +0400

    changing order of the imports, tabs and conditional expressions

--- app/lib/mp/RoomInfo.js
+++ app/lib/mp/RoomInfo.js
@@ -2,30 +2,29 @@ import React, {Component} from 'react';
 import {connect} from 'react-redux';
 
 function mapStateToProps(state) {
-    return {room: state.activeRoom}
-}
+  return {room: state.activeRoom}
+};
 
 class RoomInfo extends Component {
-    render() {
-        if (this.props.room) {
-            return (
-                <div className='room-info'>
-                    <div className='room-name'>
-                        {this.props.room.name}
-                    </div>
-                    <div className='room-details-container'>
-                        <div className='room-description'>
-                            Description: {this.props.room.descr}
-                        </div>
-                        <div className='room-image-container'>
-                            <img className='room-image' src={this.props.room.icon}/>
-                        </div>
-                    </div>
-                </div>
-            )
-        }
+  render() {
+    if (this.props.room) {
+      return (
+        <div className= 'room-info'>
+          <div className='room-name'>
+            {this.props.room.name}
+          </div>
+          <div className={this.props.room.index !== 0 ? 'room-details-container' : ''}>
+            <div className='room-description'>
+                Description: {this.props.room.descr}
+            </div>
+            <div className='room-image-container'>
+                <img className='room-image' src={this.props.room.icon}/>
+            </div>
+          </div>
+        </div>
+      )
     }
+  }
 }
-
 
 export default connect(mapStateToProps)(RoomInfo);
--- app/lib/mp/RoomList.js
+++ app/lib/mp/RoomList.js
@@ -1,69 +1,72 @@
 import React, {Component} from 'react';
-import {connect} from 'react-redux';
+
 import {roomActivate} from '../../actions/roomActivate.action.js';
+import {connect} from 'react-redux';
+
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import MenuItem from 'material-ui/MenuItem';
 import Paper from 'material-ui/Paper';
 import Menu from 'material-ui/Menu';
 
 const paperstyle = {
-    width: '212px',
-    margin: 0,
-    overflow: 'hidden',
-    height: '100%'
+  width: 212,
+  margin: 0,
+  overflow: 'hidden',
+  height: '100%'
 };
 
 function mapStateToProps(state) {
-    return {rooms: state.rooms}
+  return {rooms: state.rooms}
 };
 
 class RoomList extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      value: 0
+    };
+  };
 
-    constructor(props) {
-        super(props);
-        this.state = {
-            value: 0
-        }
+  componentWillMount() {
+    if (this.props.rooms) {
+      this.props.roomActivate(this.props.rooms[0])
     }
+  };
 
-    componentWillMount() {
-        if (this.props.rooms) {
-            this.props.roomActivate(this.props.rooms[0])
-        }
-    }
+  selectRow = (room, index) => {
+    this.setState({value: index})
+    this.props.roomActivate(room)
+  };
 
-    selectRow = (room, index) => {
-        this.setState({value: index})
-        this.props.roomActivate(room)
-    }
+  renderingMenuItems() {
+    return this.props.rooms.map((room, index) => {
+      return (
+        <div key={room.index} className = {room.class}>
+          <MenuItem
+            key={room.index}
+            className={room.index === this.state.value ? 'selected' : ''}
+            onClick={() => this.selectRow(room, index)}
+            leftIcon={<div className = {room.color}/>}>
+              <p>{room.name}</p>
+          </MenuItem>
+        </div>
+      )
+    })
+  };
 
-    renderingMenuItems() {
-        return this.props.rooms.map((room, index) => {
-            return (
-              <div key={room.index} className = {room.class}>
-                <MenuItem key={room.index} className={room.index === this.state.value
-                    ? 'selected'
-                    : ''} onClick={() => this.selectRow(room, index)} leftIcon={< div className = {
-                    room.color
-                } > </div>}><p>{room.name}</p></MenuItem>
-              </div>
-            )
-        })
-    }
-
-    render() {
-        return (
-            <MuiThemeProvider>
-                <div>
-                    <Paper style={paperstyle}>
-                        <Menu value={this.state.value}>
-                            {this.renderingMenuItems()}
-                        </Menu>
-                    </Paper>
-                </div>
-            </MuiThemeProvider>
-        )
-    }
+  render() {
+    return (
+      <MuiThemeProvider>
+        <div>
+          <Paper style={paperstyle}>
+            <Menu value={this.state.value}>
+              {this.renderingMenuItems()}
+            </Menu>
+          </Paper>
+        </div>
+      </MuiThemeProvider>
+    )
+  }
 }
 
 export default connect(mapStateToProps, {roomActivate})(RoomList);
